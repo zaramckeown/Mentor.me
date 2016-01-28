7a640d9299dae2b56be70b2759ed69ccbdf4176c
nickbodd <nick.bodd90@gmail.com>
2016-01-28 13:51:00 -0800

    material design date bug fixed
    date bug with angular materials hacked

--- modules/users/client/controllers/settings/edit-profile.client.controller.js
+++ modules/users/client/controllers/settings/edit-profile.client.controller.js
@@ -3,23 +3,45 @@
 angular.module('users').controller('EditProfileController', ['$scope', '$http', '$location', 'Users', 'Authentication',
   function ($scope, $http, $location, Users, Authentication) {
     $scope.user = Authentication.user;
+    console.log($scope.user);
 
-    $scope.user.profile.education = [];
-    $scope.user.profile.experience = [];
-    $scope.user.profile.awards = [];
-    $scope.user.profile.interests = [];
-    $scope.user.profile.links = [];
+    $scope.eduD = {};
+    $scope.eduD.sdt = [];
+    $scope.eduD.edt = [];
+    for (var i = 0; i < $scope.user.profile.education.length; i += 1) {
+      $scope.eduD.sdt.push({ dt : new Date($scope.user.profile.education[i].startDate) });
+      $scope.eduD.edt.push({ dt : new Date($scope.user.profile.education[i].endDate) });
+    }
+
+    $scope.expD = {};
+    $scope.expD.sdt = [];
+    $scope.expD.edt = [];
+    for (i = 0; i < $scope.user.profile.experience.length; i += 1) {
+      $scope.expD.sdt.push({ dt : new Date($scope.user.profile.experience[i].startDate) });
+      $scope.expD.edt.push({ dt : new Date($scope.user.profile.experience[i].endDate) });
+    }
+
+    $scope.awardD = {};
+    $scope.awardD.sdt = [];
+    for (i = 0; i < $scope.user.profile.awards.length; i += 1) {
+      $scope.awardD.sdt.push({ dt : new Date($scope.user.profile.awards[i].date) });
+    }
 
     $scope.addNewEducation = function() {
-      $scope.user.profile.education.push({ schoolName: '', description : '', startDate : '', endDate : '', courseTitle : '' });
+      $scope.user.profile.education.push({ schoolName: '', description : '', startDate : new Date(), endDate : new Date(), courseTitle : '' });
+      $scope.eduD.sdt.push({ dt : new Date($scope.user.profile.education[$scope.user.profile.education.length - 1].startDate) });
+      $scope.eduD.edt.push({ dt : new Date($scope.user.profile.education[$scope.user.profile.education.length - 1].endDate) });
     };
 
     $scope.addNewExperience = function() {
-      $scope.user.profile.experience.push({ company : '', description : '', startDate : '', endDate : '' });
+      $scope.user.profile.experience.push({ company : '', description : '', startDate : new Date(), endDate : new Date() });
+      $scope.expD.sdt.push({ dt : new Date($scope.user.profile.experience[$scope.user.profile.experience.length - 1].startDate) });
+      $scope.expD.edt.push({ dt : new Date($scope.user.profile.experience[$scope.user.profile.experience.length - 1].endDate) });
     };
 
     $scope.addNewAward = function() {
-      $scope.user.profile.awards.push({ title : '', description : '', issuer : '', date : '' });
+      $scope.user.profile.awards.push({ title : '', description : '', issuer : '', date : new Date() });
+      $scope.awardD.sdt.push({ dt : new Date($scope.user.profile.awards[$scope.user.profile.awards.length - 1].date) });
     };
 
     $scope.addNewInterest = function() {
@@ -30,13 +52,24 @@ angular.module('users').controller('EditProfileController', ['$scope', '$http', 
       $scope.user.profile.links.push({ url : '' });
     };
 
-    $scope.addNewEducation();
-    $scope.addNewExperience();
-    $scope.addNewAward();
-    $scope.addNewInterest();
-    $scope.addNewLink();
+    if ($scope.user.profile.education.length === 0) {
+      $scope.addNewEducation();
+    }
 
-    console.log($scope.user.profile);
+    if ($scope.user.profile.experience.length === 0) {
+      $scope.addNewExperience();
+    }
+
+    if ($scope.user.profile.awards.length === 0) {
+      $scope.addNewAward();
+    }
+
+    if ($scope.user.profile.interests.length === 0) {
+      $scope.addNewInterest();
+    }
+    if ($scope.user.profile.links.length === 0) {
+      $scope.addNewLink();
+    }
 
     // Update a user profile
     $scope.updateUserProfile = function (isValid) {
@@ -49,6 +82,12 @@ angular.module('users').controller('EditProfileController', ['$scope', '$http', 
       }
 
       var user = new Users($scope.user);
+
+      user.profile.education = $scope.user.profile.education;
+      user.profile.experience = $scope.user.profile.experience;
+      user.profile.interests = $scope.user.profile.interests;
+      user.profile.links = $scope.user.profile.links;
+      user.profile.awards = $scope.user.profile.awards;
 
       user.$update(function (response) {
         $scope.$broadcast('show-errors-reset', 'userForm');
