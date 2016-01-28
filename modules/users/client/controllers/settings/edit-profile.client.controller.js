822571ca42313dce79120b72ac01a63259070654
nickbodd <nick.bodd90@gmail.com>
2016-01-28 10:32:11 -0800

    initial working version of edit profile page
    empty fields display only with data binding

--- modules/users/client/controllers/settings/edit-profile.client.controller.js
+++ modules/users/client/controllers/settings/edit-profile.client.controller.js
@@ -4,6 +4,40 @@ angular.module('users').controller('EditProfileController', ['$scope', '$http', 
   function ($scope, $http, $location, Users, Authentication) {
     $scope.user = Authentication.user;
 
+    $scope.user.profile.education = [];
+    $scope.user.profile.experience = [];
+    $scope.user.profile.awards = [];
+    $scope.user.profile.interests = [];
+    $scope.user.profile.links = [];
+
+    $scope.addNewEducation = function() {
+      $scope.user.profile.education.push({ schoolName: '', description : '', startDate : '', endDate : '', courseTitle : '' });
+    };
+
+    $scope.addNewExperience = function() {
+      $scope.user.profile.experience.push({ company : '', description : '', startDate : '', endDate : '' });
+    };
+
+    $scope.addNewAward = function() {
+      $scope.user.profile.awards.push({ title : '', description : '', issuer : '', date : '' });
+    };
+
+    $scope.addNewInterest = function() {
+      $scope.user.profile.interests.push('');
+    };
+
+    $scope.addNewLink = function() {
+      $scope.user.profile.links.push('');
+    };
+
+    $scope.addNewEducation();
+    $scope.addNewExperience();
+    $scope.addNewAward();
+    $scope.addNewInterest();
+    $scope.addNewLink();
+
+    console.log($scope.user.profile);
+
     // Update a user profile
     $scope.updateUserProfile = function (isValid) {
       $scope.success = $scope.error = null;
