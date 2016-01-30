dd4e2565d95bf2a904210b2944e086f7ffbbd49f
Zara <mckeown-z@email.ulster.ac.uk>
2016-01-30 16:19:53 +0000

    profile

--- modules/core/client/config/core-profile.client.menus.js
+++ modules/core/client/config/core-profile.client.menus.js
@@ -1,12 +1,12 @@
 'use strict';
 
 // Configuring the Articles module
-angular.module('profile').run(['Menus',
+angular.module('core.profile').run(['Menus',
   function (Menus) {
     // Add the articles dropdown item
     Menus.addMenuItem('topbar', {
       title: 'Search Mentors',
-      state: 'profile',
+      state: 'profile.search',
       type: 'item',
       roles: ['admin', 'student', 'mentor']
     });
