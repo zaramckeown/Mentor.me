235fbc91ca97109c21e18e3caa592a91a19ac0aa
Zara <mckeown-z@email.ulster.ac.uk>
2016-02-03 22:05:18 +0000

    removing unneeded method

--- modules/questions/server/models/comments.server.model.js
+++ modules/questions/server/models/comments.server.model.js
@@ -34,49 +34,6 @@ var CommentSchema = new Schema({
   }
 });
 
-CommentSchema.methods.upvote = function (user, callback) {
-  // If this user hasnom't upvoted yet:
-  if (this.usersWhoUpvoted.indexOf(user._id) === -1) {
-    this.usersWhoUpvoted.push(user._id);
-    this.upvotes++;
-
-    // If this user has downvoted, revert the downvote:
-    if (this.usersWhoDownvoted.indexOf(user._id) !== -1) {
-      this.usersWhoDownvoted.splice(this.usersWhoDownvoted.indexOf(user._id), 1);
-      this.downvotes--;
-    }
-
-    this.save(callback);
-  } else {
-    // TODO this violates idempotency of PUT, we should have another PUT method for reverting an upvote - or does it?
-    this.usersWhoUpvoted.splice(this.usersWhoUpvoted.indexOf(user._id), 1);
-    this.upvotes--;
-
-    this.save(callback);
-  }
-};
-
-CommentSchema.methods.downvote = function (user, callback) {
-  if (this.usersWhoDownvoted.indexOf(user._id) === -1) {
-    this.usersWhoDownvoted.push(user._id);
-    this.downvotes++;
-
-    // If this user has upvoted, revert the upvote:
-    if (this.usersWhoUpvoted.indexOf(user._id) !== -1) {
-      this.usersWhoUpvoted.splice(this.usersWhoUpvoted.indexOf(user._id), 1);
-      this.upvotes--;
-    }
-
-    this.save(callback);
-  } else {
-    // TODO this violates idempotency of PUT, we should have another PUT method for reverting an upvote
-    this.usersWhoDownvoted.splice(this.usersWhoDownvoted.indexOf(user._id), 1);
-    this.downvotes--;
-
-    this.save(callback);
-  }
-};
-
 mongoose.model('Comments', CommentSchema);
 var deepPopulate = require('mongoose-deep-populate')(mongoose);
 CommentSchema.plugin(deepPopulate);
