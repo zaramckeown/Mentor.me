bc3bb22632c0eaae7f9fbbb67d533eca70081aa4
Zara <mckeown-z@email.ulster.ac.uk>
2016-02-03 22:36:42 +0000

    making view profile

--- modules/users/client/controllers/viewProfile.client.controller.js
+++ modules/users/client/controllers/viewProfile.client.controller.js
@@ -7,6 +7,29 @@ angular.module('core.profile').controller('DisplayUserProfileController', ['$sco
     var path = '/api/users/' + $stateParams.userId;
     $http.get(path).success(function(data) {
       $scope.user = data;
+
+      $scope.eduD = {};
+      $scope.eduD.sdt = [];
+      $scope.eduD.edt = [];
+
+      for (var i = 0; i < $scope.user.profile.education.length; i += 1) {
+        $scope.eduD.sdt.push({ dt : new Date($scope.user.profile.education[i].startDate) });
+        $scope.eduD.edt.push({ dt : new Date($scope.user.profile.education[i].endDate) });
+      }
+
+      $scope.expD = {};
+      $scope.expD.sdt = [];
+      $scope.expD.edt = [];
+      for (i = 0; i < $scope.user.profile.experience.length; i += 1) {
+        $scope.expD.sdt.push({ dt : new Date($scope.user.profile.experience[i].startDate) });
+        $scope.expD.edt.push({ dt : new Date($scope.user.profile.experience[i].endDate) });
+      }
+
+      $scope.awardD = {};
+      $scope.awardD.sdt = [];
+      for (i = 0; i < $scope.user.profile.awards.length; i += 1) {
+        $scope.awardD.sdt.push({ dt : new Date($scope.user.profile.awards[i].date) });
+      }
     });
   }
 ]);
