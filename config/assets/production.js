11a6728cae824a812dfa75a3323ae60e6068a5b3
Zara <mckeown-z@email.ulster.ac.uk>
2016-05-04 18:13:50 -0400

    production update

--- config/assets/production.js
+++ config/assets/production.js
@@ -4,19 +4,34 @@ module.exports = {
   client: {
     lib: {
       css: [
-        'public/lib/bootstrap/dist/css/bootstrap.min.css',
-        'public/lib/bootstrap/dist/css/bootstrap-theme.min.css',
+        'public/lib/bootstrap/dist/css/bootstrap.css',
+        'public/lib/bootstrap/dist/css/bootstrap-theme.css',
+        'public/lib/angular-material/angular-material.css',
+        'public/lib/ng-dialog/css/ngDialog-theme-default.min.css',
+        'public/lib/ng-dialog/css/ngDialog.min.css',
+        'public/lib/ng-dialog/css/ngDialog-theme-plain.min.css',
+        'public/lib/angular-advanced-searchbox/dist/angular-advanced-searchbox.min.css',
+        'public/lib/components-font-awesome/css/font-awesome.min.css',
+        'public/lib/wowjs/css/libs/animate.css'
       ],
       js: [
-        'public/lib/angular/angular.min.js',
-        'public/lib/angular-resource/angular-resource.min.js',
-        'public/lib/angular-animate/angular-animate.min.js',
-        'public/lib/angular-messages/angular-messages.min.js',
-        'public/lib/angular-ui-router/release/angular-ui-router.min.js',
-        'public/lib/angular-ui-utils/ui-utils.min.js',
-        'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js',
-        'public/lib/angular-file-upload/angular-file-upload.min.js',
-        'public/lib/owasp-password-strength-test/owasp-password-strength-test.js'
+        'public/lib/jquery/dist/jquery.js',
+        'public/lib/angular/angular.js',
+        'public/lib/angular-aria/angular-aria.js',
+        'public/lib/angular-resource/angular-resource.js',
+        'public/lib/angular-animate/angular-animate.js',
+        'public/lib/angular-material/angular-material.js',
+        'public/lib/angular-messages/angular-messages.js',
+        'public/lib/angular-ui-router/release/angular-ui-router.js',
+        'public/lib/angular-ui-utils/ui-utils.js',
+        'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
+        'public/lib/angular-file-upload/angular-file-upload.js',
+        'public/lib/owasp-password-strength-test/owasp-password-strength-test.js',
+        'public/lib/lodash/dist/lodash.js',
+        'public/lib/angularjs-dropdown-multiselect/src/angularjs-dropdown-multiselect.js',
+        'public/lib/ng-dialog/js/ngDialog.js',
+        'public/lib/angular-advanced-searchbox/dist/angular-advanced-searchbox-tpls.min.js',
+        'public/lib/wowjs/dist/wow.min.js'
       ]
     },
     css: 'public/dist/application.min.css',
