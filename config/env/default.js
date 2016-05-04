22e88f9f3479c550a4bb53c8186aed8cd8a5515c
Zara <mckeown-z@email.ulster.ac.uk>
2016-05-04 13:47:23 -0400

    removing logo

--- config/env/default.js
+++ config/env/default.js
@@ -28,8 +28,6 @@ module.exports = {
   // for obsecurity reasons
   sessionKey: 'sessionId',
   sessionCollection: 'sessions',
-  logo: 'modules/core/client/img/brand/logo.png',
-  favicon: 'modules/core/client/img/brand/favicon.ico',
   uploads: {
     profileUpload: {
       dest: './modules/users/client/img/profile/uploads/', // Profile upload destination path
